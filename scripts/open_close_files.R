f7eea13a67b47a23cac691a7edc9b545a6a36fb7
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2022-10-27 22:03:16 -0400

    script to open file out of rstudio and also close it

---
+++ scripts/open_close_files.R
@@ -0,0 +1,54 @@
+# to examine all tasks open on your machine ----------------
+system("tasklist /?")
+# to close a program ---------------------------------------
+system("taskkill /?")
+
+# /FI - filter
+# WINDOWTITLE - the call to filter for window
+# eq = equal
+# <name of file> - Excel = actual name of the window when its opened
+# /F = forcibly
+# /V = verbose
+# *NOTE*: this will kill all excel pages because
+    # of ShellExperienceHost.exe (jump list for excel)
+    # this .exe I guess allows jumping between excel files, but only
+    # the most active one will be able to be searched
+
+# ---- to find ----
+# system('tasklist /FI "WINDOWTITLE eq <file-name> - Excel" /V')
+
+# ---- active (i.e. last to view) ----
+# Image Name                     PID Session Name        Session#    Mem Usage
+# ========================= ======== ================ =========== ============
+#   EXCEL.EXE                    29816 Console                    1    384,528 K
+
+# ---- non-active ----
+# INFO: No tasks are running which match the specified criteria.
+
+# ---- to kill ----
+# system('taskkill /FI "WINDOWTITLE eq <file-name> - Excel" /F')
+
+# create two txt files
+library(magrittr)
+tibble::tribble(~x, 1, 2, 2,3) %>%
+  write.csv("./tsst.txt")
+
+tibble::tribble(~x, 1, 2, 2,3) %>%
+  write.csv("./test.txt")
+
+# open both outside of rstudio
+shell.exec(here::here("tsst.txt"))
+shell.exec(here::here("test.txt"))
+
+# finds both open .txt files 
+system('tasklist /FI "IMAGENAME eq notepad.exe" /V')
+
+# finds the file called tsst (2-ways)
+system('tasklist /FI "WINDOWTITLE eq tsst - Notepad" /V')
+system('tasklist /FI "WINDOWTITLE eq tsst - *" /V') # * is wildcard
+
+# will kill only tsst files
+system('taskkill /FI "WINDOWTITLE eq tsst - Notepad*" /F')
+
+
+
