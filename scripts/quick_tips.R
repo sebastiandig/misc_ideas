7ef5c543662ad32674e7dc69f6b5b70d5f9b4d33
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2022-11-02 13:39:21 -0400

    added latex example

--- scripts/quick_tips.R
+++ scripts/quick_tips.R
@@ -1,8 +1,45 @@
 
-# make line with text
-header1 <- cli::rule(
-  left = crayon::bold("Something important"),
-  right = crayon::blurred("something else"))
-header2 <- cli::rule(center = crayon::yellow("Hello World"))
-print(header1)
-print(header2)
+# ---- wrap code in {} ----  
+# will run as block, instead of line by line
+{
+  print(c(1,2,3))
+  plot(cars, main = "Stopping Distance versus Speed")
+  lines(stats::lowess(cars))
+}
+
+# ---- make line with text as output to console ----
+{
+  header1 <- cli::rule(
+    left = crayon::bold("Something important"),
+    right = crayon::blurred("something else"))
+  header2 <- cli::rule(center = crayon::yellow("Hello World"))
+  print(header1)
+  print(header2)
+}
+
+# ---- print LaTeX in plots tab ----
+# save old par
+oldpar <- par(no.readonly = TRUE)
+
+{ 
+  # example latex
+  ex   <-  c(
+    paste0("$a = \\frac {a \\times(x \\cdot b^T)}",
+           "{a \\times(b \\cdot b^T) + 10^{-100}}$"),
+    paste0("$b = \\frac{b \\times (a^T \\cdot x + b_0)}",
+           "{(a^T \\cdot(a \\cdot b) + b) + 10^{-100}}$"))
+  x    <- c(0.5, 0.5) # set location of x on plot at middle
+  y    <- c(0.75, 0.25) # set location of y on plot at 75% and 25% of 1
+  plot.new() # start new plot
+  par(mar = c(0, 0, 0, 0)) # set par for margins
+  plot.window(xlim = c(-0.1, 1), ylim = c(0, 1)) # size plot window
+  # print strings of LaTeX
+  text(x, y, stringr::str_c("latex2exp::TeX(r\"(", ex, ")\")"), 
+       pos = 3, cex = 0.7, family = "mono")
+  # print LaTeX
+  text(x, y, latex2exp::TeX(ex), pos = 1, cex = 0.75)
+  
+  # return old par
+  par(oldpar)
+}
+
