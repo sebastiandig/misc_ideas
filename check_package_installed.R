2a76861f994b2ffdf0f9166d524703620788af86
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2022-11-04 11:17:32 -0400

    create function and test script

---
+++ check_package_installed.R
@@ -0,0 +1,27 @@
+
+# check if package is installed, and install if not
+
+is_inst <- function(pkg, prt = FALSE) {
+  pkg <- as.data.frame(cbind(package = pkg, has = NA))
+  for (i in 1:nrow(pkg)) {
+    pkg[i, 2] <- nzchar(system.file(package = pkg[i, 1]))
+    
+  }
+  if (prt) print(pkg)
+  
+  # if has package 
+  return(pkg)
+}
+
+packes <- c("bestglm", "fitdistrplus", "NADA", "survminer", "car", "Kendall",
+            "nlme", "vegan", "cenGAM", "mgcv", "perm", "EnvStats", "multcomp", "lattice",
+            "rms", "npsurvSS", "reshape2", "Hmisc", "ggplot2", 
+            "jmuOutlier", ## has permutation bases t-test
+            "MXM", ## Has permutation based correlation matrix
+            "gridExtra", "egg", "gtable", "ggplot2", "plyr", "scales", "readxl")
+
+packes_has <- is_inst(packes, TRUE) 
+packes_no <- packes_has[packes_has$has == FALSE,]
+install.packages(packes_no[,1])
+
+print(is_inst(packes))
