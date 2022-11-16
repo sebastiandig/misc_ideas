1411e944c18760dbe464171fe2e7a1f37f1e0afa
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2022-11-16 15:57:03 -0500

    added a tip for using with()

--- scripts/quick_tips.R
+++ scripts/quick_tips.R
@@ -108,6 +108,42 @@ oldpar <- par(no.readonly = TRUE)
 }
 
 # ============================================================================ #
+# ---- with() ----
+# ============================================================================ #  
+# similar to attach, except you don't need to attach the variable to call its
+# column names
+# allows to use the information by name inside a variable
+# this is sort of how dplyr works with `.` <dot notation>
+{
+  with(mtcars, mpg[cyl == 8  &  disp > 350])
+  # is the same as, but nicer than
+  mtcars$mpg[mtcars$cyl == 8  &  mtcars$disp > 350]
+  
+  with(data.frame(u = c(5,10,15,20,30,40,60,80,100),
+                  lot1 = c(118,58,42,35,27,25,21,19,18),
+                  lot2 = c(69,35,26,21,18,16,13,12,12)),
+       list(summary(glm(lot1 ~ log(u), family = Gamma)),
+            summary(glm(lot2 ~ log(u), family = Gamma))))
+  
+  # instead of 
+  x <-
+    data.frame(u = c(5,10,15,20,30,40,60,80,100),
+               lot1 = c(118,58,42,35,27,25,21,19,18),
+               lot2 = c(69,35,26,21,18,16,13,12,12))
+  summary(glm(lot1 ~ log(u), x, family = Gamma))
+  summary(glm(lot2 ~ log(u), x, family = Gamma))
+  
+  dplyr::select(x, lot1) |> # still a data.frame
+    dplyr::pull()           # now a vector
+  x$lot1
+  with(x, lot1)
+  
+  rm(x)
+} 
+
+
+
+# ============================================================================ #
 # ---- read multiple sheets from excel ----
 # ============================================================================ #  
 # Need path, then read sheet, map over each sheet from the path, then conver to
