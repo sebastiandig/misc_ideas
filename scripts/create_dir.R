54b5e9ecfef9760e9b3baa2d98b91dfda698275c
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2023-08-04 18:30:10 -0400

    create function to create directory

--- scripts/create_dir.R
+++ scripts/create_dir.R
@@ -1,39 +1,43 @@
 # will create a set directory if does not exists
 # useful for new projects
 
-showTree <- function(showtree = TRUE) {
+showTree <- function(.loc, showtree = TRUE, ...) {
   if (showtree) {
     cli::cli_text("Here is the {.strong project} structure:")
-    fs::dir_tree(type = "directory")
+    fs::dir_tree(.loc, type = "directory", ...)
   }
 }
 
-subDir_deflt <- c(
-    "data/raw",
-    "data/processed",
-    "data/plots",
-    "data/metadata",
-    "Rmd",
-    "scripts"
-)
-
-cust_dir <- c(
-
-)
-subDir <- here::here(c(subDir_deflt, cust_dir))
-subDir <- subDir[which(!fs::dir_exists(subDir))]
-
-fs::dir_create(path = subDir)
-
-if (!length(subDir) == 0) {
+create_dir <- function(
+    .loc     = NULL, 
+    cust_dir = NULL,
+    showtree = FALSE, ...) {
+  
+  subDir_deflt <- c(
+    here::here(.loc, "data", c("raw", "processed", "plots", "metadata")),
+    here::here(.loc, "Rmd"),
+    here::here(.loc, "scripts")
+  )
+  
+  subDir <- c(subDir_deflt, cust_dir)
+  subDir <- subDir[which(!fs::dir_exists(subDir))]
+  
+  fs::dir_create(path = subDir)
+  
+  if (!length(subDir) == 0) {
     cli::cli_alert_info("Creating {length(subDir)} director{?y/ies}.")
-    cli::cli_ul(subDir, .close = T)
-    showTree(showtree = TRUE)
-    } else {
-        cli::cli_alert_info("No new directories created.")
-        }
+    cli::cli_ul(subDir, .close = TRUE)
+    showtree <- TRUE
+  } else {
+    cli::cli_alert_info("No new directories created.")
+  }
+  
+  showTree(.loc = .loc, showtree = showtree, ...)
+  
+  # ---- end of function create_dir
+}
 
-rm(subDir_deflt, cust_dir, subDir)
 
-
-
+create_dir(here::here(),
+           showtree = TRUE,
+           recurse = 2)
