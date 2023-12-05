f115de8e727135da49b0c551c0ebf85dca6649a6
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2023-12-05 11:05:48 -0500

    update

--- scripts/calculation_tips.R
+++ scripts/calculation_tips.R
@@ -38,15 +38,16 @@
 #' upwell(ektrx, ektry, coast_angle)
 #' 
 upwell <- function(ektrx, ektry, coast_angle) {
-  pi <- 3.1415927
+  pi       <- 3.1415927
   degtorad <- pi/180.
-  alpha <- (360 - coast_angle) * degtorad
-  s1 <- cos(alpha)
-  t1 <- sin(alpha)
-  s2 <- -1 * t1
-  t2 <- s1
-  perp <- (s1 * ektrx) + (t1 * ektry)
-  para <- (s2 * ektrx) + (t2 * ektry)
+  alpha    <- (360 - coast_angle) * degtorad
+  s1       <- cos(alpha)
+  t1       <- sin(alpha)
+  s2       <- -1 * t1
+  t2       <- s1
+  perp     <- (s1 * ektrx) + (t1 * ektry)
+  para     <- (s2 * ektrx) + (t2 * ektry)
+  
   return(perp/10)
 }
 
